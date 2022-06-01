048b8a3c6895ccb126c97c32f0718e8d40047153
Mark Rogers <mark.rogers@pieinsurance.com>
2022-06-01 13:05:50 -0600

    Improve readme and handle casing

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
-# random-semantly
+# random-semantle
 Generate a new Semantle and Team Code
 
+## How To Run
+1. Activate the Virtual Environment
+   1. Create virtual environment: `python3 -m venv venv`
+   2. Activate the virtual environment: `source venv/bin/activate`
+   3. Install dependencies: `pip3 install -r requirements.txt`
+2. Run script: `python3 main.py`
 
-## English Word List
 
-Source: https://github.com/first20hours/google-10000-english/blob/master/google-10000-english-usa.txt
-> I removed all 1-letter words
+## Notes about the list of English Words
+
+The `words.txt` file was sourced from [this github page](https://github.com/first20hours/google-10000-english/blob/master/google-10000-english-usa.txt).
+
+Nota bene: I modified this source file in the following ways:
+1. I removed all 1-letter words (including "i" and "a")
--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import base64
 from random import randint
 
 WORD_LIST_FILENAME = "words.txt"
-SEMANTLE_BASE_URL = "https://semantle.com/nearby_1k/"
+SEMANTLE_BASE_URL = "https://semantle.com/?word="
+ENCODING = "utf-8"
 
 
 def _pick_random_word() -> str:
     with open(WORD_LIST_FILENAME) as word_list_file:
-        word_list = [word.strip() for word in word_list_file]
+        word_list = [word.strip().lower() for word in word_list_file]
     max_range = len(word_list)
     random_idx = randint(0, max_range-1)
     random_word = word_list[random_idx]
@@ -17,14 +18,14 @@ def _pick_random_word() -> str:
 def _base64_word(word: str, number_suffix: int = None) -> str:
     if not number_suffix:
         number_suffix = randint(1337, 999999)
-    str_to_encode = f"{word}{number_suffix}".encode("utf-8")
+    str_to_encode = f"{word}{number_suffix}".encode(ENCODING)
     encoded_bytes = base64.b64encode(str_to_encode)
-    encoded_str = str(encoded_bytes, "utf-8")
+    encoded_str = str(encoded_bytes, ENCODING)
     return encoded_str
 
 
 def _get_url_for_semantle(base64_word_and_suffix: str) -> str:
-    url = f"https://semantle.com/?word={base64_word_and_suffix}"
+    url = f"{SEMANTLE_BASE_URL}{base64_word_and_suffix}"
     return url
 
 
